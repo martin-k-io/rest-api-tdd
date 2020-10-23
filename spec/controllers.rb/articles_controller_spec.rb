337ee0c7eb376a7814e173f9425ac1f1f2e571c4
Martin K <martin.k.io.github@gmail.com>
2020-10-24 00:22:06 +0100

    Add .recent scope for ordering

--- spec/controllers.rb/articles_controller_spec.rb
+++ spec/controllers.rb/articles_controller_spec.rb
@@ -10,10 +10,10 @@ RSpec.describe ArticlesController, type: :controller do
     end
 
     it 'should return proper json' do
-      articles = create_list :article, 2 #factorybot method
+      create_list :article, 2 #factorybot method
       subject
 
-      articles.each_with_index do |article, index |
+      Article.recent.each_with_index do |article, index |
         expect(json_data[index]['attributes']).to eq({
           "title" => article.title,
           "content" => article.content,
@@ -26,8 +26,8 @@ RSpec.describe ArticlesController, type: :controller do
       old_article = create :article
       newer_article = create :article
       subject
-      expect(json_data.first['id'].to eq(newer_article.id.to_s))
-      expect(json_data.last['id'].to eq(old_article.id.to_s))
+      expect(json_data.first['id']).to eq(newer_article.id.to_s)
+      expect(json_data.last['id']).to eq(old_article.id.to_s)
     end
   end
 end
