cd22754789dab857e772050ec83f5a029bd39be6
Martin K <martin.k.io.github@gmail.com>
2020-10-23 23:19:52 +0100

    Add routing for articles#show with routing spec

--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -5,6 +5,7 @@ describe 'articles routes' do
     expect(get '/articles').to route_to('articles#index')
   end
 
-  
-
+  it 'should route to articles show' do
+    expect(get '/articles/1').to route_to('articles#show', id: '1')
+  end
 end
