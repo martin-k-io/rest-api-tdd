5df2b1dbb9cebdb9648cd687b0b22c1d57d1426c
Martin K <martin.k.io.github@gmail.com>
2020-10-25 21:36:34 +0000

    Adding authorization handling and tests

--- spec/routing/access_token_spec.rb
+++ spec/routing/access_token_spec.rb
@@ -4,4 +4,8 @@ describe 'access tokens routes' do
   it 'should route to access_tokens create actions' do
     expect(post '/login').to route_to('access_tokens#create')
   end
+
+  it 'should route to access_tokens destroy action' do
+    expect(delete '/logout').to route_to('acces_tokens#destroy')
+  end
 end
