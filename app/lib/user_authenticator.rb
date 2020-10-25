a0f1a8ed25ceb4a2c2cee3159f302372d687dcb1
Martin K <martin.k.io.github@gmail.com>
2020-10-25 17:44:34 +0000

    Reuse existing user & refactor code

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -8,25 +8,40 @@ class UserAuthenticator
   end
 
   def perform
-    client = Octokit::Client.new(
-      client_id: ENV['GITHUB_CLIENT_ID'],
-      client_secret: ENV['GITHUB_CLIENT_SECRET']
-    )
-
-    # Accepts the authorization code
-    # Returns github access token as a result
-    token = client.exchange_code_for_token(@code)
-
     if token.try(:error).present?
       raise AuthenticationError
     else
-      user_client = Octokit::Client.new(access_token: token)
-      user_data = user_client.user.to_h.slice(:login, :name, :url, :avatar_url)
-      User.create(user_data.merge(provider: 'github'))
+      prepare_user
     end
   end
 
   private
+
+  def client
+    @client ||= Octokit::Client.new(
+      client_id: ENV['GITHUB_CLIENT_ID'],
+      client_secret: ENV['GITHUB_CLIENT_SECRET']
+    )
+  end
+
+  def token
+    # Accepts the authorization code
+    # Returns github access token as a result
+    @token ||= client.exchange_code_for_token(@code)
+  end
+
+  def user_data
+    @user_data ||= Octokit::Client.new(access_token: @token)
+      .user.to_h.slice(:login, :name, :url, :avatar_url)
+  end
+
+  def prepare_user
+    @user = if User.exists?(login: user_data[:login])
+      User.find_by(login: user_data[:login])
+    else
+      User.create(user_data.merge(provider: 'github'))
+    end
+  end
 
   attr_reader :code
 end
